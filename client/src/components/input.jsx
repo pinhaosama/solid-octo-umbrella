6df531ec355b26521a152336de0fcdb040bf487b
pinhaosama <gabrielpinhao1@gmail.com>
2023-08-29 20:48:00 -0700

    add input.jsx

--- client/src/components/input.jsx
+++ client/src/components/input.jsx
@@ -1,12 +1,43 @@
+import $ from 'jquery';
+import axios from 'axios';
+import FormData from 'form-data';
+import React from 'react';
+
 export default function Input(){
+
     return(
-        <form id="postInput" class="input-group">
-            <div id="inputs">
-                <input type="text" class="form-control" id="title" placeholder="Title" />
-                <textarea class="form-control" id="post" placeholder="Text"></textarea>
-                <input type="file" class="form-control" id="img" placeholder="Title" />
-            </div>
-            <button class="btn btn-outline-secondary" id="postButton" type="button">Post</button>
-        </form>
+        <div>
+            <textarea id="textupload" name='textupload' type="text" placeholder="Text"></textarea>
+            <form action="http://localhost:5000/post" method="post" encType="multipart/form-data">
+                <input type="file" id='imgUpload' name="fileupload"/>
+                <br/>
+                <input id='submitImg' type="submit"/>
+            </form>
+            <button onClick={ upload }></button>
+        </div>
     );
+
+    async function upload() {
+        var txt = $('#textupload').val();
+        console.log(txt);
+        $.ajax({
+            traditional: true,
+            url: "http://localhost:5000/post",
+            type: 'POST',
+            data: JSON.stringify(txt),
+            headers: {
+                "task": "text",
+                "Content-Type": "text/xml"
+            },
+            success: function (response) {
+                console.log(response);
+            },
+            error: function (error) {
+                console.error("Error:", error);
+            }
+        });
+        if($('#imgUpload').val() != ''){
+            $('#submitImg').click();
+        }
+    }
 }
